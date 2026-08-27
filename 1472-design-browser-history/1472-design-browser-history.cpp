5e20f05ee5d3190f06bfbf62646c0d15271af669
Ashutosh Yadav <rtpkrishna12345@gmail.com>
2026-08-27 16:15:28 +0530

    Time: 77 ms (17.1%), Space: 94.3 MB (17.69%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.cpp
@@ -0,0 +1,41 @@
+class BrowserHistory {
+public:
+    stack<string> past;
+    stack<string> future;
+    string curr;
+
+    BrowserHistory(string homepage) {
+        curr = homepage;
+    }
+
+    void visit(string url) {
+        past.push(curr);
+        curr = url;
+
+        while (!future.empty()) {
+            future.pop();
+        }
+    }
+
+    string back(int steps) {
+        while (steps > 0 && !past.empty()) {
+            future.push(curr);
+            curr = past.top();
+            past.pop();
+            steps--;
+        }
+
+        return curr;
+    }
+
+    string forward(int steps) {
+        while (steps > 0 && !future.empty()) {
+            past.push(curr);
+            curr = future.top();
+            future.pop();
+            steps--;
+        }
+
+        return curr;
+    }
+};
